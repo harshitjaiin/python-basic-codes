ac1b8402cd649465d5a42a57b08276b0bac0e74a
harshitjaiin <121127054+harshitjaiin@users.noreply.github.com>
2022-12-30 11:58:41 +0530

    Create while and for loop.py

---
+++ while and for loop.py
@@ -0,0 +1,24 @@
+#Examples of while loops in python
+
+i=1
+while(i<5):
+ print(i)              
+ i+=1
+ 
+j=1
+while(j<5): 
+   print(j * " * ")       #star pattern
+   j+=1
+
+k=4
+while(k>0):
+ print(k*" @ ")
+ k-=1
+ 
+ #Examples of for loop in python
+ 
+for i in range(5):
+     print((i+1) * " $ ")
+
+for item in range (7):
+     print((item /2 )) 
