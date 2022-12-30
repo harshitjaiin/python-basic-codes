c71e6782754ba4b38ada58135b01c9222c3211cd
harshitjaiin <121127054+harshitjaiin@users.noreply.github.com>
2022-12-30 11:56:14 +0530

    Create strings in python.py

---
+++ strings in python.py
@@ -0,0 +1,15 @@
+
+name="Harshit Jain"
+print(name)
+print(name.find(" "))    #returns index if found else -1
+print(name.find("r"))
+print(name.find("a"))
+print(name.find("x"))
+print(name.find("Jain")) 
+print(name.replace("Jain","Singh"))   #sirf print karte hue replace karega actually nhi
+print(name)
+
+print("J" in name)    #retuns a boolean this lets us find ki wo hai ya ni
+print("shit" in name)
+print("I" in name)
+
