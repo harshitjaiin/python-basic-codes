dec7d5a593eeb2a4af1f06cbe4931eba25df7f00
harshitjaiin <121127054+harshitjaiin@users.noreply.github.com>
2022-12-30 12:01:24 +0530

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,8 @@
+marks = {19,4,1,6,5,2,4,8,10,3,41}            # This is a set it has {}
+# marks[0] eroor throw karega also A set can only store unique values and also index se access nhi kar sakye so it is unordered
+for i in marks:
+ print(i)
+ 
+ 
+
+
