bb6f418bf4009b1690c2c8bbbce4bedf76efc505
harshitjaiin <121127054+harshitjaiin@users.noreply.github.com>
2022-12-30 12:01:44 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+
+marks={ "eng":95,"phy":96,"chem":97,"cs":98,"maths":99 }  #this uses a key and a value
+print(marks)
+print(marks["chem"])
+
+marks["pe"]=70    #adding pe to this dictionary data structure
+print(marks)
+
+marks["phy"]=100      # update marks for key "phy"
+print(marks)
